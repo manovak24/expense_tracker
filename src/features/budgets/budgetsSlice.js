58fe23b23f3656c5facc636c2d499fbd89c8c8b8
emagdnimMQT <mark.w.novak@gmail.com>
2021-09-14 13:10:00 -0400

    Refractored budgets slice to use redux toolkit.

--- src/features/budgets/budgetsSlice.js
+++ src/features/budgets/budgetsSlice.js
@@ -1,27 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 export const CATEGORIES = ['housing', 'food', 'transportation', 'utilities', 'clothing', 'healthcare', 'personal', 'education', 'entertainment'];
 const initialState = CATEGORIES.map(category => ({ category: category, amount: 0 }))
 
-export const editBudget = (budget) => {
-  return {
-    type: 'budgets/editBudget',
-    payload: budget
+const budgetsSlice = createSlice({
+  name:'budgets',
+  initialState: initialState,
+  reducers: {
+    editBudget: (state, action) => {
+      const category = action.payload.category;
+      const amount = action.payload.amount;
+      state.find(budget => budget.category === category).amount = amount
+    }
   }
-}
-
-const budgetsReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'budgets/editBudget':
-      const newBudgets = state.map(budget => {
-        if (budget.category === action.payload.category) {
-          return action.payload;
-        }
-        return budget;
-      })
-      return newBudgets;
-    default:
-      return state;
-  }
-}
+})
 
 export const selectBudgets = (state) => state.budgets;
-export default budgetsReducer;
+export const { editBudget } = budgetsSlice.actions;
+export default budgetsSlice.reducer;
